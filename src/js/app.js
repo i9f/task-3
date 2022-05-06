23d4a66970e0917f4e4b7d86f9e3fef9a5039129
Anastasia Shkirinets <smadenoshijian@gmail.com>
2022-05-06 14:34:41 +0600

    add accordion menu, change footer select id

--- src/js/app.js
+++ src/js/app.js
@@ -54,7 +54,7 @@ $("#select").select2({
 	dropdownCssClass: "form-select"
 });
 
-$("#select1").select2({
+$("#footer-select").select2({
   minimumResultsForSearch: -1, //скрыть строку поиска
   templateResult: formatState,
   templateSelection: formatState,
@@ -67,21 +67,6 @@ const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
 const grid = document.querySelector('.cryptocurrency__grid');
 const mobile_show = 10;
 const desktop_show = 19;
-
-function ss (content) {
-  return content.map(item => {
-    return `<figure class="cryptocurrency__block 
-    ${i < mobile_show ? ``: 
-      i < desktop_show ? `cryptocurrency__block_mobile-hidden`:
-      `cryptocurrency__block_desktop-hidden`}" 
-      tabindex="0"><img src="${item.image}" alt="${item.symbol} icon"> 
-      <figcaption class="cryptocurrency__block-content">
-        <span class="cryptocurrency__block-title">${item.symbol}</span>
-        <span class="cryptocurrency__block-text">${item.name}</span>
-      </figcaption>
-    </figure>`
-  })
-}
 
 async function getResponse() {
   const response = await fetch(url);
@@ -109,75 +94,20 @@ getResponse()
 
 
 //accordion
-// const btn = document.getElementsByClassName("questions__btn");
-// const text = document.getElementsByClassName("questions__btn-droptext");
+const btns = document.querySelectorAll(".questions__btn");
 
-// for (let i = 0; i < btn.length; i++) {
-//   btn[i].onclick = function() {
-//     this.classList.toggle("questions__btn_opened");
-//     const droptext = this.nextElementSibling;
-//     if (droptext.style.maxHeight) {
-//       droptext.style.cssText = `margin-bottom: 0;`;
-//       droptext.style.maxHeight = null;
-//     } else {
-//       droptext.style.cssText = `margin-bottom: 19px;`;
-//       droptext.style.maxHeight = droptext.scrollHeight + "px";
-//     }
-//   };
-// }
+btns.forEach((btn) => {
+  btn.onclick = function() {
+    hideAll();
 
-// function hideAcordian() {
-//   var accordian_content = document.getElementsByClassName("questions__btn-droptext");
-//   // var accordian_content_p = document.getElementsByTagName("p");
-//   for (var i = 0; i < accordian_content.length; i++){
-//     accordian_content[i].style.height = "0";
-//     accordian_content[i].style.visibility = "hidden";
-//     // accordian_content_p[i].style.opacity = "0";
-//   }
-// }
-
-// document.getElementsByClassName("questions__wrapper").onclick = function(event) {
-//   const target = event.target;
-//   if (target.className == 'questions__btn') {
-//     const header = document.getElementsByClassName("questions__btn");
-//     const accordian_content = document.getElementsByClassName("questions__btn-droptext");
-//     const accordian_content_p = document.getElementsByTagName("p");
-//     for (var i = 0; i < header.length; i++) {
-//       if (header[i] == target) {
-//         if (accordian_content[i].style.visibility == "hidden") {
-//           hideAcordian();
-//           accordian_content[i].style.height = "100px";
-//           accordian_content[i].style.visibility = "visible";
-//           accordian_content_p[i].style.opacity = "1";
-//         } else {
-//           hideAcordian();
-//         }
-//       }
-//     }
-//   }
-// }
-
-const acc_btns = document.querySelectorAll(".questions__btn");
-const acc_contents = document.querySelectorAll(".questions__btn-droptext");
-
-acc_btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    acc_contents.forEach((acc) => {
-      if (e.target.nextElementSibling !== acc && acc.classList.contains("active")) {
-        acc.classList.remove("active");
-        acc_btns.forEach(btn => btn.classList.remove("active"));
-      }
-    });
-
-    const panel = btn.nextElementSibling;
-    panel.classList.toggle("active");
-    btn.classList.toggle("active");
-  });
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
+  }
 });
 
-window.onclick = (e) => {
-  if (!e.target.matches(".questions__btn")) {
-    acc_btns.forEach((btn) => btn.classList.remove("active"));
-    acc_contents.forEach((acc) => acc.classList.remove("active"));
+function hideAll() {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+    btn.nextElementSibling.classList.remove("active");
   }
-};
+)};
